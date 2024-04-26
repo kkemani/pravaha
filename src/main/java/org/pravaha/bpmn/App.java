2e3657405d800fde5f469525f4eebb796b07d751
bablu.gupta <bablu.gupta@seygenindia.com>
2024-04-26 15:20:20 +0530

    updates

--- src/main/java/org/pravaha/bpmn/App.java
+++ src/main/java/org/pravaha/bpmn/App.java
@@ -13,17 +13,6 @@ public class App
 {
     public static void main( String[] args )
     {
-    	
-    	BpmnUtil bpmnUtil = new BpmnUtil();
-    	try {
-			String str = bpmnUtil.getBPMNFile("src/main/resources/SampleBpmnProcess.bpmn");
-			System.out.println("----------->>>"+str);
-			BpmnConfigurationManager obj = new BpmnConfigurationManager();
-			obj.setBpmnProcessDefinition(str);
-			obj.initializeProcessConfiguration();
-			System.out.println(obj.toString());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    	System.out.println("Hello Java...");
     }
 }
